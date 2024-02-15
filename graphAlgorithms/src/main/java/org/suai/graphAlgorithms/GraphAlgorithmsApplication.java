c79f683d03cd4435bd7158edebced154845a083f
Balandox <balandox221411@mail.ru>
2024-02-15 21:33:15 +0300

    added graphAlgorithms module

---
+++ graphAlgorithms/src/main/java/org/suai/graphAlgorithms/GraphAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package org.suai.graphAlgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ graphAlgorithms/src/test/java/org/suai/graphAlgorithms/GraphAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suai.graphAlgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
